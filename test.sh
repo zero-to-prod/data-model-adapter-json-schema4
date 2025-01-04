653c791946f67cb7e84a58945e7b9004403111bf
david_smith <david_smith@sweetwater.com>
2025-01-04 14:39:30 -0500

    Update `test.sh`.

--- test.sh
+++ test.sh
@@ -4,12 +4,7 @@ set -e
 php_versions=("php83" "php82" "php81")
 
 for version in "${php_versions[@]}"; do
-  vendor_dir="./vendor-${version}"
-
-  if [ ! -d "$vendor_dir" ] || [ -z "$(ls -A "$vendor_dir")" ]; then
-    echo "Dependencies not found or vendor directory is empty for $version. Installing dependencies..."
-    docker compose run --rm "${version}composer" composer install --no-cache
-  fi
+  docker compose run --rm "${version}composer" composer update --no-cache
 
   if ! docker compose run --rm "$version" vendor/bin/phpunit; then
     echo "Tests failed on $version."
